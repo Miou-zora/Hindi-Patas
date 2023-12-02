51f87755d7ad8275d4d302504ac808d9ebdd6a8b
MatthieuGA <matthieu.guillet-arnaud@epitech.eu>
2023-12-02 14:20:28 +0100

    Version with scene tranition

---
+++ Hindi Patas/character.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+
+const SPEED = 600.0
+const JUMP_VELOCITY = -800.0
+var _animator = AnimationPlayer
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = 980
+
+func _physics_process(delta):
+	# Add the gravity.
+	#if not is_on_floor():
+	velocity.y += gravity * delta
+	# Handle jump.
+	if Input.is_action_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	_animator.PROCESS_MODE_ALWAYS
+	move_and_slide()

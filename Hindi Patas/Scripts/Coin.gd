14e280f1ac73676313d3342a7adb9bd1f864cc84
MatthieuGA <matthieu.guillet-arnaud@epitech.eu>
2023-12-02 16:11:58 +0100

    Longer level

--- Hindi Patas/Scripts/Coin.gd
+++ Hindi Patas/Scripts/Coin.gd
@@ -24,8 +24,10 @@ func coin_hover(delta):
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		AudioManager.coin_pickup_sfx.play()
+		get_tree().call_group("Player", "death_tween")
 		GameManager.add_score()
 		var tween = create_tween()
 		tween.tween_property(self, "scale", Vector2.ZERO, 0.1)
 		await tween.finished
 		queue_free()
+		get_tree().reload_current_scene()

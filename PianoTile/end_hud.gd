6bfe88503b2b50677b99025ed882a732c7f6c340
MathisCaugant <mathis.caugant@epitec.eu>
2023-12-03 11:13:35 +0100

    add: possibility to lose with HUD

--- PianoTile/end_hud.gd
+++ PianoTile/end_hud.gd
@@ -1,14 +1,19 @@
-extends RigidBody2D
-signal lose
+extends CanvasLayer
+signal replay
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	hide()
 	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func _on_visible_on_screen_notifier_2d_screen_exited():
-	emit_signal("lose")
-	queue_free()
+func _on_replay_pressed():
+	replay.emit()
+	pass # Replace with function body.
+
+func _on_quit_pressed():
+	pass # Replace with function body.
